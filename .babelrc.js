9b3af44b402536e53eb590fe18140c29cf411dc8
hekunyu <hekunyu@me.com>
2020-06-21 11:32:46 +0800

    init: clone 2.6.11 并配置alias、flow相关供跳转

---
+++ .babelrc.js
@@ -0,0 +1,24 @@
+const babelPresetFlowVue = {
+  plugins: [
+    require('@babel/plugin-proposal-class-properties'),
+    // require('@babel/plugin-syntax-flow'), // not needed, included in transform-flow-strip-types
+    require('@babel/plugin-transform-flow-strip-types')
+  ]
+}
+
+module.exports = {
+  presets: [
+    require('@babel/preset-env'),
+    "@babel/preset-flow",
+    // require('babel-preset-flow-vue')
+    babelPresetFlowVue
+  ],
+  plugins: [
+    require('babel-plugin-transform-vue-jsx'),
+    require('@babel/plugin-syntax-dynamic-import')
+  ],
+  ignore: [
+    'dist/*.js',
+    'packages/**/*.js'
+  ]
+}
---
+++ src/core/instance/index.js
@@ -0,0 +1,24 @@
+import { initMixin } from './init'
+import { stateMixin } from './state'
+import { renderMixin } from './render'
+import { eventsMixin } from './events'
+import { lifecycleMixin } from './lifecycle'
+import { warn } from '../util/index'
+
+function Vue (options) {
+  // 只能够通过 new 实例化
+  if (process.env.NODE_ENV !== 'production' &&
+    !(this instanceof Vue)
+  ) {
+    warn('Vue is a constructor and should be called with the `new` keyword')
+  }
+  this._init(options)
+}
+
+initMixin(Vue)
+stateMixin(Vue)
+eventsMixin(Vue)
+lifecycleMixin(Vue)
+renderMixin(Vue)
+
+export default Vue
